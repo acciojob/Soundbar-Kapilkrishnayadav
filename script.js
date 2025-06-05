7e5c70f564247ec088899106f54f6f45c79b604b
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-05 19:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// //your JS code here. If required.
-//   let pauseSound;
-//       function playMusic(e) {
-//         console.log();
-//         const sound = new Audio(`./${e.target.innerText}.mp3`); // path to your sound file
-//         if (typeof pauseSound === "object" && sound !== null)
-//         pauseSound.pause();
-//         pauseSound=sound;
-//         sound.play();
-//       }
+//your JS code here. If required.
+// let add="https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3";
+  let pauseSound;
+      function playMusic(e) {
+        console.log();
+        const sound = new Audio(`https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3`); // path to your sound file
+        if (typeof pauseSound === "object" && sound !== null)
+        pauseSound.pause();
+        pauseSound=sound;
+        sound.play();
+      }
