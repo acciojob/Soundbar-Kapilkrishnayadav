5365c4ecc42102b122025cafc50b40045a7e9cfd
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-05 19:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//your JS code here. If required.
-  let pauseSound;
-      function playMusic(e) {
-        console.log();
-        const sound = new Audio(`./${e.target.innerText}.mp3`); // path to your sound file
-        if (typeof pauseSound === "object" && sound !== null)
-        pauseSound.pause();
-        pauseSound=sound;
-        sound.play();
-      }
+// //your JS code here. If required.
+//   let pauseSound;
+//       function playMusic(e) {
+//         console.log();
+//         const sound = new Audio(`./${e.target.innerText}.mp3`); // path to your sound file
+//         if (typeof pauseSound === "object" && sound !== null)
+//         pauseSound.pause();
+//         pauseSound=sound;
+//         sound.play();
+//       }
